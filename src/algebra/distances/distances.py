459abe0cf9741f3cde9142fa1d4fd7918b74a894
Collado1234 <rennancollado@gmail.com>
2026-06-15 04:49:04 -0300

    Refactoring: Basis

--- src/algebra/distances/distances.py
+++ src/algebra/distances/distances.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 class Distance:
-    def __init__(self):
-        pass
-
     @staticmethod
-    def euclidean_distance(X:np.ndarray, sample:np.ndarray) -> np.ndarray
-        return np.linalg.norm(X-sample,axis=1)
-    
-    
+    def euclidean_distance(X: np.ndarray, sample: np.ndarray):
+        X = np.asarray(X, dtype=float)
+        sample = np.asarray(sample, dtype=float)
+
+        if X.ndim == 1:
+            return float(np.linalg.norm(X - sample))
+
+        return np.linalg.norm(X - sample, axis=1)
