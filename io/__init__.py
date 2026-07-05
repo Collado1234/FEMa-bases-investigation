b19485b321971da6f60af90598137a497ab14410
Collado1234 <rennancollado@gmail.com>
2026-07-05 03:30:15 -0300

    Feat: Reproducibility

--- io/__init__.py
+++ io/__init__.py
@@ -1,3 +1,7 @@
-from .loaders import __all__
-from .datasets import __all__
+from .loaders import __all__ as loaders_all
+from .datasets import __all__ as datasets_all
 
+__all__ = [
+    *loaders_all,
+    *datasets_all,
+]
