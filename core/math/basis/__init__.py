aa50b54b7bd86ff013888b153f94294151f21c29
Collado1234 <rennancollado@gmail.com>
2026-07-19 20:15:47 -0300

    Fixing __init__ basis, e voltando pasta para models

--- core/math/basis/__init__.py
+++ core/math/basis/__init__.py
@@ -1,26 +1,8 @@
-from .base_basis import BaseBasis
-from .factory_basis import Basis
-from .shepard import ShepardBasis
-from .radial import RadialBasis
-from .rbf_gaussian import RbfGaussianBasis
-from .multiquadratic import MultiquadraticBasis
-from .inverse_multiquadratic import InverseMultiquadraticBasis
-from .wendland_c2 import WendlandC2Basis
-from .quartic_spline import QuarticSplineBasis
-from .cubic_spline import CubicSplineBasis
-from .exp_gen import ExponentialGenBasis
+from .base_basis                import BaseBasis
+from .factory_basis             import Basis
+
 
 __all__ = [
     "BaseBasis",
     "Basis",
-    "ShepardBasis",
-    "RadialBasis",
-    "RbfGaussianBasis",
-    "MultiquadraticBasis",
-    "InverseMultiquadraticBasis",
-    "WendlandC2Basis",
-    "QuarticSplineBasis",
-    "CubicSplineBasis",
-    "ExponentialGenBasis",
-
 ]
