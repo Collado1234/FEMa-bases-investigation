28f336f7f2c60928590557e02ec95ac6fbf8a555
Collado1234 <rennancollado@gmail.com>
2026-07-19 18:08:06 -0300

    Feat: lorentzian basis

---
+++ core/math/basis/lorentzian_basis.py
@@ -0,0 +1,17 @@
+import numpy as np
+from .base_basis import BaseBasis
+
+class LorentzianBasis(BaseBasis):
+    def __init__(self, search):
+        super().__init__(search)
+    
+    def compute_weights(self, dists:np.ndarray, alpha:float, c:float) -> np.ndarray:
+        dists = np.where(dists == 0, 1e-10, dists) 
+
+        weights = 1 / ( 1 + dists**4 )
+
+        weights /= weights
+
+        return weights
+
+        
