138c8afeb53a7044fb619f9b97bbecebf9c82b16
Collado1234 <rennancollado@gmail.com>
2026-08-17 02:09:27 -0300

    Corrigindo Softmax

--- core/math/basis/softmax_radial.py
+++ core/math/basis/softmax_radial.py
@@ -15,6 +15,4 @@ class SoftmaxRadialBasis(BaseBasis):
 
     def evaluate(self, dists: np.ndarray, params: BasisParameters) -> np.ndarray:
         beta = self._require(params)["beta"]
-        soma = np.sum(np.exp(-beta * dists), axis=1, keepdims=True)
-
-        return np.exp((-beta * dists)/soma)
+        return np.exp(-beta * dists)
